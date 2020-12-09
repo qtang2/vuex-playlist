93c760e72e704a2b15dc9aa15357ea71fdecbff9
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-09 23:50:16 +1100

    build a simple product app which displays two different product lists

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductListOne from './components/ProductListOne'
+import ProductListTwo from './components/ProductListTwo'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  name:'app',
+  data(){
+    return{
+      products:[
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80}
+      ]
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+body{
+  font-family: Ubuntu;
+  color:#555;
 }
 </style>
