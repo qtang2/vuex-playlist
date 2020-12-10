a0f308bf4df886eea246f492902bbceb52375980
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-10 12:52:00 +1100

    use mapGetters and mapActions to map getters to computed properties, to map actions to methods in the component. Please notice, when doing the mapping, parameters is not included, but can use in store actions

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -13,21 +13,30 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+import {mapGetters} from 'vuex'
+
 export default {
   computed:{
     products(){
       return this.$store.state.products
     },
-    saleProducts(){
-      return this.$store.getters.saleProducts
-    }
+    // saleProducts(){
+    //   return this.$store.getters.saleProducts
+    // }
+    ...mapGetters([
+      'saleProducts'
+    ])
   },
   methods:{
-    reducePrice: function(amount){
-      // this.$store.commit('reducePrice')
-      //instea, dispatch an action to commit the mutation
-      this.$store.dispatch('reducePrice',amount)
-    }
+    // reducePrice: function(amount){
+    //   // this.$store.commit('reducePrice')
+    //   //instea, dispatch an action to commit the mutation
+    //   this.$store.dispatch('reducePrice',amount)
+    // }
+    ...mapActions([
+      'reducePrice'
+    ])
   }
 
 }
